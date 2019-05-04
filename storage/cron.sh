924482dbe78549fe1d189687f2e3091de0fcb1b6
Remus Bunduc <bunduc@adobe.com>
2019-05-04 23:42:28 +0300

    add cron

--- storage/cron.sh
+++ storage/cron.sh
@@ -12,10 +12,13 @@ for file in "../storage/fn/*.js"; do
     baseFile=$(basename -- ${file})
     key="${baseFile%%.*}"
     echo "Running ${key}"
+    docker-compose build
     docker-compose run --rm -e "FORCE_ADD=${FORCE_ADD}" ${key}
 done
 
 cd "$DIR/../../actor-notify-email"
-docker-compose run --rm -e "GMAIL_USER=$GMAIL_USER" -e "GMAIL_PASSWORD=$GMAIL_PASSWORD" -e "CONTAINS=Bucurestii" actor-notify-email
+docker-compose build
+docker-compose run --rm -e "GMAIL_USER=$GMAIL_USER" -e "GMAIL_PASSWORD=$GMAIL_PASSWORD" -e "CONTAINS=Bucurestii|Jiului|Damaroaia|Bazilescu" actor-notify-email
 docker-compose run --rm -e "GMAIL_USER=$GMAIL_USER" -e "GMAIL_PASSWORD=$GMAIL_PASSWORD" -e "SECTOR=1" actor-notify-email
 docker-compose run --rm -e "GMAIL_USER=$GMAIL_USER" -e "GMAIL_PASSWORD=$GMAIL_PASSWORD" -e "AD_TYPE=executare" actor-notify-email
+docker-compose run --rm -e "GMAIL_USER=$GMAIL_USER" -e "GMAIL_PASSWORD=$GMAIL_PASSWORD" actor-notify-email
