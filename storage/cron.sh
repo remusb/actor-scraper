94a4442b4f6c6b63b3135571d18a8234d2a05f78
Remus Bunduc <bunduc@adobe.com>
2019-05-04 14:50:29 +0300

    fix cron

--- storage/cron.sh
+++ storage/cron.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
-cd "$PWD/../cheerio-scraper"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$DIR/../cheerio-scraper"
 
 FORCE_ADD=0
 if [[ -n "$1" ]]; then
     FORCE_ADD=$1
 fi
 
-for file in "$PWD/../storage/fn/*.js"; do
+for file in "$DIR/../storage/fn/*.js"; do
     key=$(basename ${file%.*})
     echo "Running ${key}"
     docker-compose run --rm -e "FORCE_ADD=${FORCE_ADD}" ${key}
 done
 
-cd "$PWD/../../actor-notify-email"
+cd "$DIR/../../actor-notify-email"
 docker-compose run --rm -e "GMAIL_USER=$GMAIL_USER" -e "GMAIL_PASSWORD=$GMAIL_PASSWORD" -e "CONTAINS=Bucurestii" actor-notify-email
 docker-compose run --rm -e "GMAIL_USER=$GMAIL_USER" -e "GMAIL_PASSWORD=$GMAIL_PASSWORD" -e "SECTOR=1" actor-notify-email
 docker-compose run --rm -e "GMAIL_USER=$GMAIL_USER" -e "GMAIL_PASSWORD=$GMAIL_PASSWORD" -e "AD_TYPE=executare" actor-notify-email
