a096a91b23b6993db4433214a2ee9feb3eb7a31a
Remus Bunduc <bunduc@adobe.com>
2019-05-07 07:02:10 +0300

    finish brizaland and fix null price

--- storage/fn/brizaland-ro.js
+++ storage/fn/brizaland-ro.js
@@ -5,12 +5,18 @@ async function pageFunction(context) {
     let entries = {};
     const domain = context.customData.DOMAIN;
     const terenuriStore = await context.Apify.openKeyValueStore('terenuri' );
+    const configStore = await context.Apify.openKeyValueStore('configs' );
+    const configMap = await configStore.getValue('scrappers');
+    const maxRetry = configMap.maxRetry;
+    const ronToEur = configMap.ronToEur;
+    const minPrice = parseInt(configMap.minPrice);
+    const maxPrice = parseInt(configMap.maxPrice);
 
     if (request.userData.label === "DETAIL") {
         const entry = crawlPage(request.userData.entry);
         entries[entry.id] = entry;
         await terenuriStore.setValue(entry.id, entry);
-        // console.log(`Updated ${entry.id}`);
+        console.log(`Updated ${entry.id}`);
     } else {
         await crawlListing();
     }
@@ -18,59 +24,70 @@ async function pageFunction(context) {
     function crawlPage(entry) {
         context.skipLinks();
         entry.sector = 0;
-        entry.size = 0;
-        entry.title = $('div.titlu h1').text().trim();
 
-        $('div.header_info div div:first-child span.hidden-print').remove();
-        const locationText = $('div.header_info div div:first-child').text().trim();
-        const detaliiText = $('#b_detalii_text p').text().trim();
-
+        const detaliiText = $('div.mb30 p').text().trim().replace(/\n/g, ' ').replace(/\s\s+/g, ' ');
         let sectorInfo = detaliiText.match(/sector(ul)? (\d)/i);
         if (sectorInfo != null && sectorInfo.length > 2) {
             entry.sector = parseInt(sectorInfo[2]);
         }
+
         sectorInfo = entry.title.match(/sector(ul)? (\d)/i);
         if (sectorInfo != null && sectorInfo.length > 2) {
             entry.sector = parseInt(sectorInfo[2]);
         }
-        sectorInfo = locationText.match(/sector(ul)? (\d)/i);
+
+        entry.location = $("div.property-list dt:contains('Locatie')").next().text().trim() + ', ' + $("div.property-list dt:contains('Zona')").next().text().trim();
+        sectorInfo = entry.location.match(/sector(ul)? (\d)/i);
         if (sectorInfo != null && sectorInfo.length > 2) {
             entry.sector = parseInt(sectorInfo[2]);
         }
-        entry.location = locationText;
+
         entry.detail = detaliiText;
-        entry.size = $("ul.lista-tabelara li:contains('Suprafaţă') span").text().trim();
+        entry.size = $('div.col-sm-6:nth-child(2) .module-content span').text().trim();
 
         return entry;
     }
 
     async function crawlListing() {
-        const elems = $('div.box-anunt[id]').toArray();
+        const elems = $('.property-row').toArray();
         let skipped = 0;
         let cnt = 0;
 
         for (let i = 0; i < elems.length; i++) {
-            const $el = $(elems[i]);
-            let priceText = $('div.pret span.pret-mare', $el).text().trim();
+            const $el = elems[i];
+            let priceText = $('div.property-row-meta-item:first-child strong', $el).text().trim();
 
             // format number
-            priceText = priceText.replace(/ /g, '');
+            priceText = priceText.replace(/\s/g, '');
             priceText = priceText.replace(/\./g, '');
             priceText = priceText.replace(/,/g, '.');
-            if (priceText == null) {
-                priceText = "0";
+            if (priceText.includes("nespecificat")) {
+                continue;
             }
             let price = parseFloat(priceText);
+            if (priceText.includes("RON")) {
+                price = Math.round(price/ronToEur);
+            }
+            if (price < minPrice || price > maxPrice) {
+                log.info(`Skipping because of price: ${price}`);
+                continue;
+            }
 
+            const fullId = $('a.property-row-image', $el).attr('href');
+            const idSegments = fullId.match(/\/brizaland\/ro\/detalii\/([A-Za-z0-9\-\.]+)/i);
+            if (idSegments == null || idSegments.length < 2) {
+                log.error(`ID match issue: ${JSON.stringify(fullId)}`);
+                continue;
+            }
             const entry = {
-                id: $el.attr('id'),
-                title: '',
+                id: idSegments[1],
+                title: $('h2.property-row-title', $el).text().trim().replace(/\n/g, ' ').replace(/\s\s+/g, ' '),
                 price: price,
                 fav: false,
-                executare: $('.box-tip-licitatie', $el).length > 0,
-                notified: false
+                executare: false,
+                notified: false,
+                url: domain + $('a.property-row-image', $el).attr('href')
             };
-            entry.url = $('a[itemprop=name]', $el).attr('href');
 
             const storeEntry = await terenuriStore.getValue(entry.id);
             if (process.env.FORCE_ADD != "1" && storeEntry != null && storeEntry.price == entry.price) {
